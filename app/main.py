d20ba91e4186770bbce42bb61c426a53ee56fcf6
carlosryan300 <carlos.silva@empiricus.com.br>
2020-10-03 17:47:39 -0300

    db in memory

--- app/main.py
+++ app/main.py
@@ -22,7 +22,6 @@ def dinamico(dinamico):
     if dinamico == "qualquercoisa":
         return (f"Não existe: {dinamico}", 404)
     return (f"Eu recebi esse parâmetro dinamicamente: {dinamico}", 200)
-    (str, dict, list, numbers)
 
 
 # exemplo de chamada usando caminho/path argumentos
@@ -110,16 +109,10 @@ def index_if(nome):
     return render_template("if.html", title=title, users=users)
 
 
-# for i in a:
-#     i['url']
-
-# {% for i in a %}
-#     {{ i.url }}
-# {% endfor %}
 users_list = []
 
 
-@app.route("/add/user/")
+@app.route("/add/user/", methods=["POST"])
 def add_user():
     args = dict(request.args)
     if "username" not in args or "url" not in args:
@@ -132,3 +125,85 @@ def add_user():
 @app.route("/get/users/")
 def get_user():
     return render_template("users.html", users=users_list)
+
+
+@app.route("/file/", methods=["GET", "POST"])
+def files():
+    if request.method == "GET":
+        return render_template("form.html")
+
+    files = request.files.getlist("file")
+
+    for file in files:
+        path = f"app/file/{file.filename}"
+        file.save(path)
+
+    return ({"message": "Olá esse method é um post"}, 201)
+
+
+# criar banco de dados em memoria
+
+banco_de_dados = []
+
+
+@app.route("/pessoa/", methods=["GET"])
+@app.route("/pessoa/<int:id>/", methods=["GET"])
+def get_pessoa(id: int = None) -> dict:
+    """ 
+    consultar um id e retornar um dict caso exista, 
+    caso não ele retornar vazio e status 400 
+    """
+    if not id:
+        return {"pessoas": banco_de_dados}
+
+    for pessoa in banco_de_dados:
+        if id in pessoa:
+            return (pessoa, 200)
+    return ({"message": "id não encontrado"}, 400)
+
+
+@app.route("/pessoa/", methods=["POST"])
+def add_pessoa():
+    """ criar um registro, caso o body não seja vazio."""
+    dados = request.json
+    condition = ("nome" in dados, "idade" in dados, "altura" in dados, "cor_de_pele" in dados)
+    if not all(condition):
+        return ({"message": "dados invalidos"}, 400)
+
+    aux = 0
+    for i in banco_de_dados:
+        pivot = list(i.keys())[0]
+        if aux < pivot:
+            aux = pivot
+    id = aux + 1
+    banco_de_dados.append({id: dados})
+    return ({"message": f"usuário {dados['nome']} inserido com sucesso."}, 200)
+
+
+@app.route("/pessoa/<int:id>/", methods=["DELETE"])
+def delete_pessoa(id: int):
+    aux = None
+    for index, value in enumerate(banco_de_dados):
+        if id in value:
+            aux = index
+
+    if aux is not None:
+        result = banco_de_dados.pop(aux)
+        return ({"message": "usuário removido com sucesso", "user": result}, 200)
+
+    return ({"message": "usuário não encontrad"}, 404)
+
+
+@app.route("/pessoa/<int:id>/", methods=["PUT"])
+def update_pessoa(id: int):
+    dados = request.json
+    condition = ("nome" in dados, "idade" in dados, "altura" in dados, "cor_de_pele" in dados)
+    if not all(condition):
+        return ({"message": "dados invalidos"}, 400)
+
+    for index, value in enumerate(banco_de_dados):
+        if id in value:
+            banco_de_dados[index].update({id: dados})
+            return ({"message": "dados atualizados"}, 200)
+
+    return ({"message": "dados invalidos"}, 400)
